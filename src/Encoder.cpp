60df0ed26c97a27acd4217a2db0afbd65b340d30
Sharan Rathnakumar <sharanrk45@gmail.com>
2024-02-08 21:45:12 +0530

    [Subject] Implemented the logic to copy Image header to the Image file to be Encoded.
    [Description] The first 54 bytes, which is the Image header for the bmp files is copied to a buffer array of char pointer.
                  It is then copied to the "EncodedImage.bmp" which is the output image which will be encoded.

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -2,5 +2,48 @@
 
 int EncodeTextInToImage(char *ImageFname, char *TextFname) {
 	//TODO: Implementation of Encoder function
+	FILE *ImgPtr, *TextPtr, *EncImgPtr;
+	char * buffer[54];
+
+	ImgPtr = fopen(ImageFname, "rb");
+	TextPtr = fopen(TextFname, "rb");
+	EncImgPtr = fopen("EncodedImage.bmp","wb");
+
+	if (ImgPtr == NULL || TextPtr == NULL || EncImgPtr) {
+		std::cerr << "ERROR: Unable to open the file." << std::endl;
+		return -1;
+	}
+
+	size_t bytesRead = fread(buffer, 1, 54, ImgPtr);
+	
+	if (bytesRead != 54) {
+		std::cerr << "ERROR: Reading data from image failed" << std::endl;
+		fclose(ImgPtr);
+		fclose(TextPtr);
+		fclose(EncImgPtr);
+		return -1;
+	}
+
+	//INFO: Copying first 54 bytes [Image header]
+	size_t bytesWritten = fwrite(buffer, 1, bytesRead, EncImgPtr);
+	 if (bytesWritten != bytesRead) {
+		 std::cerr << "Error writing to output file" << std::endl;
+        fclose(ImgPtr);
+        fclose(TextPtr);
+        fclose(EncImgPtr);
+        return -1;
+    }	
+	std::cout << "INFO: Image header copied successfully" << std::endl;
+
+	fseek(TextPtr, 0, SEEK_END);
+	long TextFileSize = ftell(TextPtr);
+
+	if(TextFileSize == -1) {
+		std::cerr << "ERROR: Unable to get the size of the Text file" << std::endl;
+		return -1;
+	}
+	rewind(TextPtr);
+
 	return -1;
 }
+
